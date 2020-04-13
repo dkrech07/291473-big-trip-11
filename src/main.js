134d41b786acea8e91f3b02787c92fcd9e2cbda1
Дмитрий Кречетников <dkrech07@gmail.com>
2020-04-13 22:58:13 +0500

    Исправляет названия переменных

--- src/main.js
+++ src/main.js
@@ -32,10 +32,10 @@ const renderComponent = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const headerContainer = document.querySelector(`.page-header`);
-const tripMenu = headerContainer.querySelector(`.trip-main`);
-const tripSwitch = tripMenu.querySelector(`.trip-main__trip-controls h2:first-child`);
-const tripFilter = tripMenu.querySelector(`.trip-main__trip-controls h2:last-child`);
+const headerElement = document.querySelector(`.page-header`);
+const tripMenuElement = headerElement.querySelector(`.trip-main`);
+const tripSwitchElement = tripMenuElement.querySelector(`.trip-main__trip-controls h2:first-child`);
+const tripFilterElement = tripMenuElement.querySelector(`.trip-main__trip-controls h2:last-child`);
 
 const generateTripInfo = () => {
 
@@ -50,7 +50,7 @@ const generateTripInfo = () => {
     const tripInfo = `${firstPointDestination}`;
     const tripDate = `${getDay(firstDate)}`;
 
-    renderComponent(tripMenu, createTripInfoTemplate(tripInfo, tripDate, tripCost), `afterbegin`);
+    renderComponent(tripMenuElement, createTripInfoTemplate(tripInfo, tripDate, tripCost), `afterbegin`);
   }
 
   if (sortList.length === 2) {
@@ -60,7 +60,7 @@ const generateTripInfo = () => {
     const tripInfo = `${firstPointDestination} — ${lastPointDestination}`;
     const tripDate = `${getDay(firstDate)} — ${getDay(lastDate)}`;
 
-    renderComponent(tripMenu, createTripInfoTemplate(tripInfo, tripDate, tripCost), `afterbegin`);
+    renderComponent(tripMenuElement, createTripInfoTemplate(tripInfo, tripDate, tripCost), `afterbegin`);
   }
 
   if (sortList.length === 3) {
@@ -71,7 +71,7 @@ const generateTripInfo = () => {
     const tripInfo = `${firstPointDestination} — ${secondPointDestination} — ${lastPointDestination}`;
     const tripDate = `${getDay(firstDate)} — ${getDay(lastDate)}`;
 
-    renderComponent(tripMenu, createTripInfoTemplate(tripInfo, tripDate, tripCost), `afterbegin`);
+    renderComponent(tripMenuElement, createTripInfoTemplate(tripInfo, tripDate, tripCost), `afterbegin`);
   }
 
   if (sortList.length > 3) {
@@ -81,76 +81,76 @@ const generateTripInfo = () => {
     const tripInfo = `${firstPointDestination} ... ${lastPointDestination}`;
     const tripDate = `${getDay(firstDate)} — ${getDay(lastDate)}`;
 
-    renderComponent(tripMenu, createTripInfoTemplate(tripInfo, tripDate, tripCost), `afterbegin`);
+    renderComponent(tripMenuElement, createTripInfoTemplate(tripInfo, tripDate, tripCost), `afterbegin`);
   }
 };
 
 generateTripInfo();
 
-renderComponent(tripSwitch, createMenuTemplate(), `afterend`);
-renderComponent(tripFilter, createFiltersTemplate(), `afterend`);
+renderComponent(tripSwitchElement, createMenuTemplate(), `afterend`);
+renderComponent(tripFilterElement, createFiltersTemplate(), `afterend`);
 
-const mainContainer = document.querySelector(`.page-body__page-main`);
-const tripEvents = mainContainer.querySelector(`.trip-events`);
-renderComponent(tripEvents, createSortTemplate(), `beforeend`);
-renderComponent(tripEvents, createTripFormTemplate(), `beforeend`);
+const mainElement = document.querySelector(`.page-body__page-main`);
+const tripEventsElement = mainElement.querySelector(`.trip-events`);
+renderComponent(tripEventsElement, createSortTemplate(), `beforeend`);
+renderComponent(tripEventsElement, createTripFormTemplate(), `beforeend`);
 
-const eventHeader = mainContainer.querySelector(`.event__header`);
-const destinationsList = eventHeader.querySelector(`.event__input--destination + datalist`);
-const eventTripList = eventHeader.querySelector(`.event__type-list .event__type-group:first-child legend`);
-const eventStopList = eventHeader.querySelector(`.event__type-list .event__type-group:last-child legend`);
+const eventHeadertElement = mainElement.querySelector(`.event__header`);
+const destinationsListElement = eventHeadertElement.querySelector(`.event__input--destination + datalist`);
+const eventTripListElement = eventHeadertElement.querySelector(`.event__type-list .event__type-group:first-child legend`);
+const eventStopListElement = eventHeadertElement.querySelector(`.event__type-list .event__type-group:last-child legend`);
 
 for (const destination of DESTINATIONS) {
-  renderComponent(destinationsList, createDestinationsTemplate(destination), `afterbegin`);
+  renderComponent(destinationsListElement, createDestinationsTemplate(destination), `afterbegin`);
 }
 
 for (const tripType of TRIP_TYPES) {
-  renderComponent(eventTripList, createEventTypeTemplate(tripType), `afterend`);
+  renderComponent(eventTripListElement, createEventTypeTemplate(tripType), `afterend`);
 }
 
 for (const stopType of STOP_TYPES) {
-  renderComponent(eventStopList, createEventTypeTemplate(stopType), `afterend`);
+  renderComponent(eventStopListElement, createEventTypeTemplate(stopType), `afterend`);
 }
 
-renderComponent(eventHeader, createOffersTemplate(), `afterend`);
-const eventDetails = tripEvents.querySelector(`.event__details`);
-const eventOffes = eventDetails.querySelector(`.event__available-offers`);
-const eventDescription = eventDetails.querySelector(`.event__section-title--destination`);
-const eventPhotos = eventDetails.querySelector(`.event__photos-tape`);
+renderComponent(eventHeadertElement, createOffersTemplate(), `afterend`);
+const eventDetailsElement = tripEventsElement.querySelector(`.event__details`);
+const eventOffesElement = eventDetailsElement.querySelector(`.event__available-offers`);
+const eventDescriptionElement = eventDetailsElement.querySelector(`.event__section-title--destination`);
+const eventPhotosElement = eventDetailsElement.querySelector(`.event__photos-tape`);
 
 const renderOfferInfo = (numberDay, numberTripPoint) => {
   const tripPointInfo = randomDaysList[numberDay].wayPoints[numberTripPoint];
   const {offers, destinationInfo} = tripPointInfo;
 
   for (const offer of offers) {
-    renderComponent(eventOffes, createOfferTemplate(offer), `afterbegin`);
+    renderComponent(eventOffesElement, createOfferTemplate(offer), `afterbegin`);
   }
 
   const description = destinationInfo.destinationDescription;
-  renderComponent(eventDescription, createDescriptionTemplate(description), `afterend`);
+  renderComponent(eventDescriptionElement, createDescriptionTemplate(description), `afterend`);
 
   for (const photo of destinationInfo.destinationPhotos) {
-    renderComponent(eventPhotos, createPhotosTemplate(photo), `afterbegin`);
+    renderComponent(eventPhotosElement, createPhotosTemplate(photo), `afterbegin`);
   }
 };
 
 renderOfferInfo(0, 0);
 
-renderComponent(tripEvents, createTripDaysTemplate(), `beforeend`);
-const tripDaysContainer = mainContainer.querySelector(`.trip-days`);
+renderComponent(tripEventsElement, createTripDaysTemplate(), `beforeend`);
+const tripDaysElement = mainElement.querySelector(`.trip-days`);
 
 const renderTripDay = () => {
   const daysList = randomDaysList.slice().sort((a, b) => a.date > b.date ? 1 : -1);
 
   for (let i = 0; i < daysList.length; i++) {
-    renderComponent(tripDaysContainer, createTripDayTemplate(daysList[i]), `beforeend`);
+    renderComponent(tripDaysElement, createTripDayTemplate(daysList[i]), `beforeend`);
   }
 
-  const tripEventsList = tripDaysContainer.querySelectorAll(`.trip-events__list`);
+  const tripEventsListElements = tripDaysElement.querySelectorAll(`.trip-events__list`);
 
   for (let i = 0; i < daysList.length; i++) {
     const wayPoint = daysList[i].wayPoints;
-    const currentTripDay = tripEventsList[i];
+    const currentTripDay = tripEventsListElements[i];
     const currentDate = daysList[i];
 
     for (let j = 0; j < wayPoint.length; j++) {
@@ -163,11 +163,11 @@ const renderTripDay = () => {
 
   for (let i = 0; i < daysList.length; i++) {
     const currentDay = daysList[i];
-    const currentOffersList = daysElements[i].querySelectorAll(`.event__selected-offers`);
+    const currentOffersListElements = daysElements[i].querySelectorAll(`.event__selected-offers`);
 
     for (let j = 0; j < currentDay.wayPoints.length; j++) {
       const currentWayPoint = currentDay.wayPoints[j];
-      const curentOfferElements = currentOffersList[j];
+      const curentOfferElements = currentOffersListElements[j];
 
       for (let k = 0; k < currentWayPoint.offers.length; k++) {
         const currentOffer = currentWayPoint.offers[k];
