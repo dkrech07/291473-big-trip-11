6f725b1071a29feb539ba37a2fc9b02b33e74a48
Дмитрий Кречетников <dkrech07@gmail.com>
2020-05-01 20:48:04 +0500

    Выводит описание и фотографии

--- src/components/form.js
+++ src/components/form.js
@@ -2,7 +2,7 @@ import {correctDateFormat, getDayInfo} from '../utils/common.js';
 import AbstractSmartComponent from "./abstract-smart-component.js";
 
 const createFormTemplate = (currentPoint) => {
-  const {type, destination, offers, price, departure, arrival, favorite} = currentPoint;
+  const {type, destination, destinationInfo, offers, price, departure, arrival, favorite} = currentPoint;
   const currentTripType = type.toLowerCase();
 
   const getTripTimeInfo = (date) => {
@@ -26,8 +26,8 @@ const createFormTemplate = (currentPoint) => {
     return checkValue;
   };
 
-  const createOffersMarkup = (offersList) => {
-    return offersList.map((offer) => {
+  const createOffersMarkup = () => {
+    return offers.map((offer) => {
       return (
         `<div class="event__offer-selector">
           <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer.type}-1" type="checkbox" name="event-offer-${offer.type}" checked>
@@ -37,6 +37,20 @@ const createFormTemplate = (currentPoint) => {
             &euro;&nbsp;<span class="event__offer-price">${offer.price}</span>
           </label>
         </div>`
+      );
+    }).join(`\n`);
+  };
+
+  const createDescriptionMarkup = () => {
+    return (
+      `<p class="event__destination-description">${destinationInfo.destinationDescription}</p>`
+    );
+  };
+
+  const createPhotosMarkup = () => {
+    return destinationInfo.destinationPhotos.map((photoUrl) => {
+      return (
+        `<img class="event__photo" src="${photoUrl}" alt="Event photo">`
       );
     }).join(`\n`);
   };
@@ -116,17 +130,18 @@ const createFormTemplate = (currentPoint) => {
             <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
             <div class="event__available-offers">
-                ${createOffersMarkup(offers)}
+                ${createOffersMarkup()}
             </div>
 
           </section>
 
           <section class="event__section  event__section--destination">
             <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+            ${createDescriptionMarkup()}
 
             <div class="event__photos-container">
               <div class="event__photos-tape">
-
+              ${createPhotosMarkup()}
               </div>
             </div>
           </section>
