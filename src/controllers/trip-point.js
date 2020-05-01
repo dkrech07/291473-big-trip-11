9762f888da3c3026e2c6ddb6aa0bb5fd15e1e4ee
Дмитрий Кречетников <dkrech07@gmail.com>
2020-05-01 21:05:27 +0500

    Удаляет лишние данные

--- src/controllers/trip-point.js
+++ src/controllers/trip-point.js
@@ -1,12 +1,7 @@
 import FormComponent from '../components/form.js';
-// import OffersComponent from '../components/offers.js';
 import FormDestinationComponent from '../components/form-destination.js';
 import FormTripTypeComponent from '../components/form-trip-type.js';
-// import OfferComponent from '../components/offer.js';
-// import DescriptionComponent from '../components/offer-description.js';
-// import PhotosComponent from '../components/offer-photos.js';
-import EventComponent from '../components/event.js';
-// import EventOfferComponent from '../components/event-offer.js';
+import EventComponent from '../components/trip-point.js';
 import {DESTINATIONS, TRIP_TYPES, STOP_TYPES} from '../mock/way-point.js';
 import {RENDER_POSITION, render, replace, remove} from '../utils/render.js';
 
@@ -18,8 +13,6 @@ const renderFormParameters = (currentMainElement) => {
   const destinationsListElement = eventHeadertElement.querySelector(`.event__input--destination + datalist`);
   const eventTripListElement = eventHeadertElement.querySelector(`.event__type-list .event__type-group:first-child legend`);
   const eventStopListElement = eventHeadertElement.querySelector(`.event__type-list .event__type-group:last-child legend`);
-
-  // render(eventHeadertElement, new OffersComponent(), RENDER_POSITION.AFTEREND);
 
   for (const destinationElement of DESTINATIONS) {
     render(destinationsListElement, new FormDestinationComponent(destinationElement), RENDER_POSITION.AFTERBEGIN);
@@ -33,27 +26,6 @@ const renderFormParameters = (currentMainElement) => {
     render(eventStopListElement, new FormTripTypeComponent(stopType), RENDER_POSITION.AFTEREND);
   }
 };
-
-// Отрисовка данных о точке маршрута в форму редактирования
-// const renderOfferInfo = (currenTripElement, currentPoint) => {
-//   const {offers, destinationInfo} = currentPoint;
-//
-//   const eventDetailsElement = currenTripElement.querySelector(`.event__details`);
-//   const eventOffesElement = eventDetailsElement.querySelector(`.event__available-offers`);
-//   const eventDescriptionElement = eventDetailsElement.querySelector(`.event__section-title--destination`);
-//   const eventPhotosElement = eventDetailsElement.querySelector(`.event__photos-tape`);
-//
-//   for (const offer of offers) {
-//     render(eventOffesElement, new OfferComponent(offer), RENDER_POSITION.AFTERBEGIN);
-//   }
-//
-//   const description = destinationInfo.destinationDescription;
-//   render(eventDescriptionElement, new DescriptionComponent(description), RENDER_POSITION.AFTEREND);
-//
-//   for (const photo of destinationInfo.destinationPhotos) {
-//     render(eventPhotosElement, new PhotosComponent(photo), RENDER_POSITION.AFTERBEGIN);
-//   }
-// };
 
 export default class PointController {
   constructor(container, onDataChange) {
@@ -72,16 +44,6 @@ export default class PointController {
       render(this._container, this._eventComponent, RENDER_POSITION.BEFOREEND);
     };
     renderTripPoint();
-
-    // Отрисовка предложений в точке маршрута
-    // const renderTripOffers = () => {
-    //   for (const offer of this._point.offers) {
-    //     const currentOfferElement = this._eventComponent.getElement().querySelector(`.event__selected-offers`);
-    //
-    //     // render(currentOfferElement, new EventOfferComponent(offer), RENDER_POSITION.BEFOREEND);
-    //   }
-    // };
-    // renderTripOffers();
 
     const getFormElement = () => {
       return this._formComponent.getElement().querySelector(`form`);
