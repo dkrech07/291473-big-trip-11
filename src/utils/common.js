40dae29ab58ae532da65fc60fe6190926915799e
Дмитрий Кречетников <dkrech07@gmail.com>
2020-05-26 22:18:45 +0500

    Блокирует сохранение формы, если дата приезда меньше даты объезда

--- src/utils/common.js
+++ src/utils/common.js
@@ -6,6 +6,7 @@ const INPUT_MONTH_DAY_FORMAT = `MMM DD`;
 const INPUT_YEAR_MONTH_DAY_FORMAT = `YYYY-MM-DD`;
 const INPUT_YEAR_MONTH_DAY_TIME_FORMAT = `YYYY-MM-DDTHH:MM`;
 const INPUT_TIME_FORMAT = `HH:mm`;
+const INPUT_DATE_FORMAT = `DD/MM/YYYY HH:mm`;
 
 const TRIP_TYPES = [
   `Taxi`, `Bus`, `Train`, `Ship`, `Transport`, `Drive`, `Flight`
@@ -50,6 +51,10 @@ const correctDateISOFormat = (date) => {
 
 const correctTimeFormat = (time) => {
   return moment(time).format(INPUT_TIME_FORMAT);
+};
+
+const correctDateAndTimeFormat = (date) => {
+  return moment(date).format(INPUT_DATE_FORMAT);
 };
 
 const calculateTripDuration = (departure, arrival) => {
@@ -116,6 +121,7 @@ export {
   correctMonthAndDayFormat,
   correctDayFormat,
   correctTimeFormat,
+  correctDateAndTimeFormat,
   getPrice,
   calculateTripTime,
   INPUT_YEAR_MONTH_DAY_FORMAT,
